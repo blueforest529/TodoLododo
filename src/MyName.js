de08919865ecad0c7505831671ba317a52f1b361
imcheong <imcheong001@gmail.com>
2019-07-10 16:28:17 +0900

    수정

--- src/MyName.js
+++ src/MyName.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-Ôªøimport React, { Component } from 'react';
-=======
 import React, { Component } from 'react';
->>>>>>> 0d7e3cecbfdd07b0b5d445e08e784d14e4ac5cdd
-
 class MyName extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        æ»≥Á is <b>{this.props.name}</b>.
-=======
-        æ»≥Á«œººø‰! <b>{this.props.name}</b> ƒøπ‘ ¿‘¥œ¥Ÿ.
->>>>>>> 0d7e3cecbfdd07b0b5d445e08e784d14e4ac5cdd
+        ????? <b>{this.props.name}</b> ???
       </div>
     );
   }
