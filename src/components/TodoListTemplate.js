0d6869a4a6d79fe006cdd62f29d05dd9ed338544
imcheong <imcheong001@gmail.com>
2019-07-12 17:38:30 +0900

    업무의 우선 순위를 정해주는 프로젝트 시작

--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -5,7 +5,7 @@ const TodoListTemplate = ({form, children}) => {
   return (
     <main className="todo-list-template">
       <div className="title">
-        오늘 할 일
+        일의 우선순위 정하기
       </div>
       <section className="form-wrapper">
         {form}
