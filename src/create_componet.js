81772ceef911b3f568a954d7b475e92abe35b9d9
imcheong <imcheong001@gmail.com>
2019-08-11 19:32:14 +0900

    주말엔 리액트 공부..

---
+++ src/create_componet.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './create_component.css';
+
+class create_component extends Component{
+    render(){
+        return {
+         //왜 react import가 안될까?
+        }
+    }
+}
+
+export default create_component
